fb4c51d50e35337dd34772e03e10d62b0b06c5af
MatiasG19 <m.galusek@gmail.com>
2023-04-09 15:51:50 +0200

    Fix building project

--- src/main.ts
+++ src/main.ts
@@ -1,51 +1,73 @@
 import * as core from '@actions/core'
-import fs, { Dirent } from 'fs';
-import path from 'path';
-import { signAabFile, signApkFile } from './sign';
+import fs, {Dirent} from 'fs'
+import path from 'path'
+import {signAabFile, signApkFile} from './sign'
 
 function findReleaseFile(releaseDir: string): Dirent[] {
-  return fs.readdirSync(releaseDir, { withFileTypes: true })
+  return fs
+    .readdirSync(releaseDir, {withFileTypes: true})
     .filter(item => !item.isDirectory())
-    .filter(item => item.name.endsWith(".apk") || item.name.endsWith(".aab"));
+    .filter(item => item.name.endsWith('.apk') || item.name.endsWith('.aab'))
 }
 
 async function run(): Promise<void> {
   try {
-    const buildDir = core.getInput('buildDirectory') ?? 'build';
-    const output = core.getInput('output') ?? path.join('build', 'signed');
+    const buildDir = core.getInput('buildDirectory') ?? 'build'
+    const output = core.getInput('output') ?? path.join('build', 'signed')
 
-    const releaseDirs = core.getInput('releaseDirectory').split('\n').filter(it => it !== '');
-    const signingKeyBase64 = core.getInput('signingKeyBase64');
-    const alias = core.getInput('alias');
-    const keyStorePassword = core.getInput('keyStorePassword');
-    const keyPassword = core.getInput('keyPassword');
-    const signingKey = path.join(buildDir, 'signingKey.jks');
-    fs.writeFileSync(signingKey, signingKeyBase64, 'base64');
+    const releaseDirs = core
+      .getInput('releaseDirectory')
+      .split('\n')
+      .filter(it => it !== '')
+    const signingKeyBase64 = core.getInput('signingKeyBase64')
+    const alias = core.getInput('alias')
+    const keyStorePassword = core.getInput('keyStorePassword')
+    const keyPassword = core.getInput('keyPassword')
+    const signingKey = path.join(buildDir, 'signingKey.jks')
+    fs.writeFileSync(signingKey, signingKeyBase64, 'base64')
     if (!fs.existsSync(output)) {
-      fs.mkdirSync(output, { recursive: true });
+      fs.mkdirSync(output, {recursive: true})
     }
     for await (const releaseDir of releaseDirs) {
-      const releaseFiles = findReleaseFile(releaseDir);
+      const releaseFiles = findReleaseFile(releaseDir)
       for await (const releaseFile of releaseFiles) {
         if (releaseFile !== undefined) {
-          const releaseFilePath = path.join(releaseDir, releaseFile.name);
-          let signedReleaseFile = '';
+          const releaseFilePath = path.join(releaseDir, releaseFile.name)
+          let signedReleaseFile = ''
           if (releaseFile.name.endsWith('.apk')) {
-            signedReleaseFile = await signApkFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
+            signedReleaseFile = await signApkFile(
+              releaseFilePath,
+              signingKey,
+              alias,
+              keyStorePassword,
+              keyPassword
+            )
           } else if (releaseFile.name.endsWith('.aab')) {
-            signedReleaseFile = await signAabFile(releaseFilePath, signingKey, alias, keyStorePassword, keyPassword);
+            signedReleaseFile = await signAabFile(
+              releaseFilePath,
+              signingKey,
+              alias,
+              keyStorePassword,
+              keyPassword
+            )
           } else {
-            core.error('No valid release file to sign, abort.');
-            core.setFailed('No valid release file to sign.');
+            core.error('No valid release file to sign, abort.')
+            core.setFailed('No valid release file to sign.')
           }
-          fs.copyFileSync(signedReleaseFile, path.join(output, signedReleaseFile.split(/(\\|\/)/g).pop() ?? releaseFile.name))
+          fs.copyFileSync(
+            signedReleaseFile,
+            path.join(
+              output,
+              signedReleaseFile.split(/(\\|\/)/g).pop() ?? releaseFile.name
+            )
+          )
         } else {
-          core.error("No release file (.apk or .aab) could be found. Abort.");
-          core.setFailed('No release file (.apk or .aab) could be found.');
+          core.error('No release file (.apk or .aab) could be found. Abort.')
+          core.setFailed('No release file (.apk or .aab) could be found.')
         }
       }
     }
-  } catch (error) {
+  } catch (error: any) {
     core.setFailed(error.message)
   }
 }
